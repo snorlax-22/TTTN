19f4613f5d75437ff5874069867c7125e2485bd4
Ngo Manh Cuong <manhcuong25710@gmail.com>
2022-06-15 09:39:47 +0700

    update toy filter Brand

--- wwwroot/public/js/toy.js
+++ wwwroot/public/js/toy.js
@@ -9,23 +9,48 @@ function showFilter(obj) {
         else {
             c[i].style.display = "block";
         }
-        
+
     }
 }
 
 function showFilteritem(obj) {
     var c = document.getElementsByClassName("filter-show");
-    
-        if (c[0].style.display == "block") {
-            c[0].style.display = "none";
-        }
-        else {
-            c[0].style.display = "block";
-        }
 
-    
+    if (c[0].style.display == "block") {
+        c[0].style.display = "none";
+    }
+    else {
+        c[0].style.display = "block";
+    }
 }
 
+$(document).ready(function () {
+    $('.c-btnbox').click(function () {
+        var searchBrand = $(this).attr('data-brand');
+        console.log(searchBrand)
+        $.ajax({
+            url: '/Product/SearchFilter',
+            type: 'GET',
+            data: {
+                searchBrand: searchBrand
+            },
+            dataType: 'json',
+            contentType: 'application/json;charset=utf-8',
+            success: function (response) {
+                var data = JSON.parse(response);
+                console.log(data)
+                
+                $('#toy-grid').load('Product/LoadToyBoxes', {
+                    aPList: data
+                    });
+                
+            },
+            error: function () {
+
+            }
+        })
+    })
+});
 
 
 
